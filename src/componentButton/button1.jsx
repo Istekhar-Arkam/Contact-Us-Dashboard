90ec9967996e0991942d8e7e39f51389431ff0f4
istekhar <amreennaaz888@gmail.com>
2024-09-08 16:42:57 +0530

    all set ui

--- src/componentButton/button1.jsx
+++ src/componentButton/button1.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 const Button1 = (props) => {
   return (
-    <button class="flex items-center justify-center gap-2 sm:gap-4relative py-2 rounded-md bg-white isolation-auto z-10 border-2 border-black before:absolute before:w-full before:transition-all before:duration-700 before:hover:w-full before:-left-full before:hover:left-0 before:rounded-full before:bg-black before:-z-10 before:aspect-square before:hover:scale-150 overflow-hidden before:hover:duration-700 hover:text-white mb-4 min-w-52 font-semibold">
+    <button class="flex items-center justify-center gap-2 sm:gap-4 relative py-2 rounded-md bg-white isolation-auto z-10 border-2 border-black before:absolute before:w-full before:transition-all before:duration-700 before:hover:w-full before:-left-full before:hover:left-0 before:rounded-full before:bg-black before:-z-10 before:aspect-square before:hover:scale-150 overflow-hidden before:hover:duration-700 hover:text-white mb-4 min-w-52 font-semibold">
       {props.icon}{props.text}
       
     </button>
